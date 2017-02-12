090dbd1a163f20a8a568857f92e1280f8813bb80
polyanskiyit <polyanskiyit@gmail.com>
2017-02-12 19:36:47 +0200

    first

---
+++ Homework_1_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введiть радiус кола:");
+            double R = Convert.ToDouble(Console.ReadLine());
+
+            Radius radius = new Radius();
+
+            Console.WriteLine(
+                radius.RadiusOfTheCircle(R)
+                );
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ Homework_1_2/Radius.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_1_2
+{
+    class Radius
+    {
+        public double RadiusOfTheCircle(double Radius)
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+}
