2d4ace899eb499cb197c4df79bd62971ebb40c70
polyanskiyit <polyanskiyit@gmail.com>
2017-02-24 22:41:58 +0200

    6

--- Homework_1_2/Program.cs
+++ Homework_1_2/Program.cs
@@ -1,26 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Homework_1_2
 {
     class Program
     {
+        private static double radiusOfTheCircle(double rad)
+        {
+            return Math.PI * Math.Pow(rad, 2);
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Введiть радiус кола:");
-            double R = Convert.ToDouble(Console.ReadLine());
-
-            Radius radius = new Radius();
-
-            Console.WriteLine(
-                radius.RadiusOfTheCircle(R)
-                );
-
+            double r = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine(radiusOfTheCircle(r));
             Console.Read();
-
         }
     }
 }
